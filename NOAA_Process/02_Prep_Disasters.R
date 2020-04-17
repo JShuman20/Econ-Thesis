a4193cf64f41929ab9ffea4de8c6d04e221c869b
Jacob Shuman <jacobshuman@Jacobs-MacBook-Pro-2.local>
2020-04-17 11:15:09 -0400

    Cleaning and Final Versions of Summary Statistics

---
+++ NOAA_Process/02_Prep_Disasters.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(readxl)
+library(writexl)
+library(rvest)
+library(xml2)
+library(data.table)
+library(zoo)
+library(purrr)
+
+#STATE CODES
+STATE = read_xlsx("~/Google Drive/DATA/ECON/STATES.xlsx") 
+
+STATE_REGION = read_xlsx("~/Google Drive/DATA/ECON/STATES_REGIONS.xlsx") %>%
+  rename(STATE = `State Code`) %>%
+  left_join(STATE, by = "STATE") %>%
+  rename(STATE_CODE = STATE,
+         STATE = `State Name`) %>%
+  mutate(STATE = toupper(STATE))
+#Read in NOAA Data
+STORM_FILES  = list.files("~/Google Drive/DATA/ECON/RAW/NOAA_Storms/")
+#Reading in All Data Files
+DAT = map_dfr(.x = STORM_FILES,
+              .f = ~ fread(str_c("~/Google Drive/DATA/ECON/RAW/NOAA_Storms/",.x)) %>%
+                mutate(DAMAGE_CROPS = as.character(DAMAGE_CROPS)))
+#Writing a File With all Merged, but no cleaning
+write_csv(DAT,"~/Google Drive/DATA/ECON/CLEAN/NOAA/01_NOAA_MERGED.csv")
+#Preliminary Cleaning
+DAT_CLEAN = DAT %>%
+  select(-c(ends_with("NARRATIVE"))) %>%
+  mutate(EVENT_TYPE = toupper(EVENT_TYPE),
+         DAMAGE_PROPERTY = as.character(DAMAGE_PROPERTY),
+         DAMAGE_PROPERTY = case_when(
+             str_detect(DAMAGE_PROPERTY, "M") ~ as.numeric(str_extract(DAMAGE_PROPERTY, pattern = "[[:digit:]]{0,4}[.]{0,1}[[:digit:]]{0,4}"))*1000000,
+             str_detect(DAMAGE_PROPERTY, "B") ~ as.numeric(str_extract(DAMAGE_PROPERTY, pattern = "[[:digit:]]{0,4}[.]{0,1}[[:digit:]]{0,4}"))*1000000000,
+             str_detect(DAMAGE_PROPERTY, "K") ~ as.numeric(str_extract(DAMAGE_PROPERTY, pattern = "[[:digit:]]{0,4}[.]{0,1}[[:digit:]]{0,4}"))*1000,
+             str_detect(DAMAGE_PROPERTY, "0") ~ as.numeric(str_extract(DAMAGE_PROPERTY, pattern = "[[:digit:]]{0,4}[.]{0,1}[[:digit:]]{0,4}"))*0),
+         DAMAGE_PROPERTY = as.numeric(DAMAGE_PROPERTY),
+         BEGIN_YEARMONTH = as.character(BEGIN_YEARMONTH),
+         YEAR = str_sub(BEGIN_YEARMONTH, 1,4),
+         MONTH = str_sub(BEGIN_YEARMONTH,5,6),
+         BEGIN_DATE = as.Date(str_c(str_sub(BEGIN_YEARMONTH,1,4), str_sub(BEGIN_YEARMONTH,5,6),BEGIN_DAY, sep = "-")),
+         BEGIN_DATE_YM = (str_c(str_sub(BEGIN_YEARMONTH,1,4), str_sub(BEGIN_YEARMONTH,5,6), sep = "-")),
+         BEGIN_DATE_YM = as.yearmon(BEGIN_DATE_YM),
+         END_DATE = as.Date(str_c(str_sub(END_YEARMONTH,1,4), str_sub(END_YEARMONTH,5,6), END_DAY, sep = "-")),
+         MID_DATE = BEGIN_DATE + floor(as.numeric(END_DATE - BEGIN_DATE)/2))
+#Filtering to US States Only
+DAT_CLEAN = DAT_CLEAN %>%
+  left_join(STATE_REGION, by = "STATE") %>%
+  filter(!is.na(Region))
+
+#Adding Variables For Placebo Group
+PLACEBO_EVENTS = c("WINTER STORM", "HEAVY SNOW","WINTER WEATHER","COLD/WIND CHILL",
+                   "BLIZZARD", "EXTREME COLD/WIND CHILL","FROST/FREEZE","ICE STORM",
+                   "LAKE-EFFECT SNOW","FREEZING FOG","HAIL FLOODING", "HAIL/ICY ROADS", "HIGH SNOW")
+DAT_CLEAN = DAT_CLEAN %>%
+  select(c(1:16,18,20:26,28,30,33,44:58)) %>%
+  mutate(PLACEBO_EVENT = ifelse(EVENT_TYPE %in% PLACEBO_EVENTS, 1,0))
+  
+
+#Writing Intermediate - 2 File
+write_csv(DAT_CLEAN, "~/Google Drive/DATA/ECON/CLEAN/NOAA/02_NOAA_CLEANED.csv")
+
---
+++ NOAA_Process/SUM_STATS_NOAA.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+library(xtable)
+library(zoo)
+
+#Read in Data
+DAT_CLEAN = read.csv("~/Google Drive/DATA/ECON/CLEAN/NOAA/02_NOAA_CLEANED.csv") 
+DAT_CLEAN = mutate(DAT_CLEAN, BEGIN_DATE_YM = as.yearmon(BEGIN_DATE_YM),
+                   YEAR = as.numeric(str_sub(BEGIN_YEARMONTH,1,4))) %>%
+  filter(YEAR %in% 1990:2018)
+
+#Number of Observations
+nrow(DAT_CLEAN)
+#Number of Unique Categories
+unique(DAT_CLEAN$EVENT_TYPE)
+#Number with Positive Damages
+table(DAT_CLEAN$DAMAGE_PROPERTY >0)[2]/nrow(DAT_CLEAN)
+#Range of Values
+range(DAT_CLEAN$DAMAGE_PROPERTY, na.rm = TRUE)
+
+
+#Identifying Above Median Damage States Over Time
+ABOVE_MED_STATES = DAT_CLEAN %>%
+  group_by(STATE) %>%
+  summarise(LOG_TOT_PROP = log(1 + sum(DAMAGE_PROPERTY,na.rm = TRUE))) %>%
+  mutate(ABOVE_MED = ifelse(LOG_TOT_PROP > median(LOG_TOT_PROP),1,0)) %>%
+  filter(ABOVE_MED == 1) %>%
+  pull(STATE)
+
+#Plotting Time Trends By Above Median
+DAT_CLEAN %>%
+  group_by(BEGIN_DATE_YM, STATE)  %>%
+  summarize(TOT_PROP = sum(DAMAGE_PROPERTY, na.rm = TRUE)) %>%
+  mutate(LOG_TOT = log(TOT_PROP + 1),
+         `Above Median` = ifelse(STATE %in% ABOVE_MED_STATES, TRUE,FALSE)) %>%
+  group_by(`Above Median`, BEGIN_DATE_YM) %>%
+  summarise(TOT_PROP = sum(TOT_PROP),
+            LOG_TOT = log(TOT_PROP + 1))  %>%
+  ggplot() + 
+  geom_line(aes(x = BEGIN_DATE_YM, y = LOG_TOT, group = `Above Median`, col = `Above Median`)) +
+  theme_minimal() +
+  theme(legend.position = "bottom") +
+  xlab("Time") + ylab("Log-Total Property Damage") +
+  ggtitle("Log-Total Property Damage By Month") 
+   
+#Plotting Some Within-State Histograms
+DAT_CLEAN %>%
+  filter(STATE %in% c("MAINE", "NORTH DAKOTA", "DELAWARE", "UTAH", "FLORIDA")) %>%
+  group_by(STATE, BEGIN_DATE_YM) %>%
+  summarize(TOT_PROP = sum(DAMAGE_PROPERTY, na.rm = TRUE)) %>%
+  mutate(LOG_TOT = log(TOT_PROP + 1)) %>%
+  ggplot() + 
+  geom_histogram(aes(x = LOG_TOT), bins = 20, fill = "black") +
+  facet_grid(~STATE) +
+  theme_bw() +
+  xlab("Monthly Log-Total Property Damage") +
+  ylab("Count") +
+  ggtitle("Distribution of Monthly Property Damages For Selected States")
+
+
+
+#Appendix Tables
+#Types of Storms that Cause Damage
+DAT_CLEAN %>%
+  #filter(DAMAGE_PROPERTY > 0) %>%
+  group_by(EVENT_TYPE) %>%
+  summarize(Count = n())%>%
+  arrange(desc(Count)) %>%
+  mutate(EVENT_TYPE = tolower(EVENT_TYPE)) %>%
+  rename(`Storm Class` = EVENT_TYPE) %>%
+  filter(row_number() <= 10)  %>%
+  xtable(., caption = "Most Frequent Types of Storms", type = "latex") %>%
+  print(., file = "~/Desktop/ECON Thesis/OUTPUT/DATA_Section/Appendix/DamageStats.tex", append = TRUE)
+    
+
+DAT_CLEAN %>%
+  filter(DAMAGE_PROPERTY > 0) %>%
+  group_by(EVENT_TYPE) %>%
+  summarize(DAMAGE = sum(DAMAGE_PROPERTY,na.rm = TRUE)/1000000) %>%
+  arrange(desc(DAMAGE)) %>%
+  mutate(EVENT_TYPE = tolower(EVENT_TYPE)) %>%
+  rename(`Storm Class` = EVENT_TYPE) %>%
+  filter(row_number() <= 10) %>%
+  xtable(., caption = "Most Damaging Types of Storms") %>%
+  print(., file = "~/Desktop/ECON Thesis/OUTPUT/DATA_Section/Appendix/DamageStats.tex", append = TRUE)
+  
+
+
+
+           
