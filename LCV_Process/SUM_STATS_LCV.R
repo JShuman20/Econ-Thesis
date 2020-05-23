2e9364ccdb76e18f1cd55b5aaf65f092f86fe6b0
Jacob Shuman <jacobshuman@Jacobs-MacBook-Pro-2.local>
2020-05-23 19:21:44 -0400

    Finished Commenting

--- LCV_Process/SUM_STATS_LCV.R
+++ LCV_Process/SUM_STATS_LCV.R
@@ -1,11 +1,21 @@
-library(tidyverse)
-library(readxl)
-library(foreign)
+#-----------------------------------------------------------------------------------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------------------------------------------------#
+#This Script Computes Univariate Summary Statistics and Figures For Senate and House Voting Data------------------------------------------------------#
+#-----------------------------------------------------------------------------------------------------------------------------------------------------#
 
+#-------------------------Loading Required Libraries---------------------#
+#For proessing
+library(tidyverse)
+library(ggplot2)
+library(dplyr)
+
+#---------------------Reading in Cleaned Data Generated From Script 1--------------------#
 SEN = read.csv("~/Google Drive/DATA/ECON/CLEAN/LCV/SENATE.csv")
 HOUSE = read.csv("~/Google Drive/DATA/ECON/CLEAN/LCV/HOUSE.csv")
 
-#Trends By Party
+
+
+#-----------------------Trends in LCV Scores By Party-------------------------#
 group.colors = c(D = "blue", R = "red", I = "darkgreen")
 
 SEN %>%
@@ -18,6 +28,20 @@ SEN %>%
   theme(legend.position = "bottom") +
   xlab("Year") + ylab("Average LCV Score") +
   ggtitle("LCV Scores By Party Over Time")
+
+#Same Figure but removing independents
+SEN %>%
+  filter(Party != "I") %>%
+  group_by(Party, Year) %>%
+  summarise(SCORE = mean(Current.Score, na.rm = TRUE)) %>%
+  ggplot() +
+  geom_line(aes(x = Year, y = SCORE, col = Party)) +
+  scale_color_manual(values=group.colors) +
+  theme_minimal() +
+  theme(legend.position = "bottom") +
+  xlab("Year") + ylab("Average LCV Score") +
+  ggtitle("LCV Scores By Party Over Time")
+
 
 #Note: Trend Looks Similar in the House -- Appendix Figure
 HOUSE %>%
@@ -32,18 +56,22 @@ HOUSE %>%
   ggtitle("LCV Scores By Party Over Time")
 
        
-#Individual Trends Over Time for Notable, Long-Serving Senators
+#---------------------------Individual Trends Over Time for Notable, Long-Serving Senators---------------------------------#
 People = c("Biden, Joe", "Conrad, Kent", "Hatch, Orrin", "Collins, Susan")
-group.colors = c("Biden, Joe" = "blue4", "Conrad, Kent" = "lightblue", "Hatch, Orrin" = "red4", "Collins, Susan" = "red2")
+group.colors = c("Biden, Joe" = "blue4", "Conrad, Kent" = "forestgreen", "Hatch, Orrin" = "red4", "Collins, Susan" = "red2")
 SEN %>%
   filter(Member.of.Congress %in% People) %>%
   ggplot() +
-  geom_line(aes(x = Year, y = Current.Score, group = Member.of.Congress, col = Member.of.Congress)) +
+  geom_line(aes(x = Year, y = Current.Score, group = Member.of.Congress, col = Member.of.Congress, lty = Member.of.Congress)) +
   theme_minimal() +
   theme(legend.position = "bottom") +
+  scale_colour_manual("", values = c("blue2", "darkorange", "purple", "red")) +
+  scale_linetype_manual("", values=c(1,2,5,6)) +
   xlab("Year") + ylab("Average LCV Score") +
   ggtitle("Yearly LCV Scores for Long-Serving Senators")
-#
+
+
+#Computing Mean and Standard Deviation in LCV Scores for These Four Senators
 SEN %>%
   filter(Member.of.Congress %in% People) %>%
   group_by(Year, Member.of.Congress) %>%
@@ -51,14 +79,16 @@ SEN %>%
   group_by(Member.of.Congress) %>%
   summarise(BIG_MEAN = mean(Mean),
             SD = sd(Mean))
-#Percentage of Voters in Middle 60%
-#Let's Restrict to post-1990
+
+#--------------Calculate the Percentage of Reps with Scores in Middle 60 Points----------------#
+#For Senate
 Mod_Sen = SEN %>%
   filter(Year > 1989) %>%
   group_by(Year, Member.of.Congress) %>%
   summarize(Mean = mean(Current.Score)) %>%
   mutate(Moderate = ifelse(Mean %in% 20:80, 1,0)) 
 table(Mod_Sen$Moderate)[2]/nrow(Mod_Sen)
+#For House
 Mod_House = HOUSE %>%
   filter(Year > 1989) %>%
   group_by(Year, Member.of.Congress) %>%
@@ -67,7 +97,7 @@ Mod_House = HOUSE %>%
 table(Mod_House$Moderate)[2]/nrow(Mod_House)
 
 
-#Number of Votes Per Year
+#-----------------------------------Number of Votes Per Year--------------------------------#
 HOUSE %>%
   distinct(Year, ROLL_CALL) %>%
   group_by(Year) %>%
@@ -87,9 +117,5 @@ SEN %>%
   ggtitle("Number of Environmental Votes Per Year - Senate") +
   theme_bw() + 
   theme(panel.border = element_blank()) 
-
-
-
-unique(SEN$Year)
 
 
