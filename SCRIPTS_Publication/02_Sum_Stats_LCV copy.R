2a1e08a548756b9923524009830bc921c4f4e3ed
Jacob Shuman <jacobshuman@Jacobs-MacBook-Pro-2.local>
2020-09-07 10:46:58 -0400

    Updated Line Graph Code

--- SCRIPTS_Publication/02_Sum_Stats_LCV copy.R
+++ SCRIPTS_Publication/02_Sum_Stats_LCV copy.R
@@ -8,15 +8,70 @@
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+library(rlang)
 
 #---------------------Reading in Cleaned Data Generated From Script 1--------------------#
 SEN = read.csv("~/Google Drive/DATA/ECON/CLEAN/LCV/SENATE.csv")
 HOUSE = read.csv("~/Google Drive/DATA/ECON/CLEAN/LCV/HOUSE.csv")
+Combined = map_dfr(.x = c("SEN", "HOUSE"),
+                   .f = ~ eval(parse_expr(.x)) %>%
+                     dplyr::select(Party, Year, POS_VOTE, ROLL_CALL, Member.of.Congress) %>%
+                     mutate(CHAMBER = .x))
+glimpse(SEN)
+#----------------------------Summary Figures------------------------------------------#
+#Number of Votes By Year
+Combined %>% group_by(Year, CHAMBER) %>% summarise(COUNT = length(unique(ROLL_CALL))) %>%
+  arrange(CHAMBER, COUNT) %>%
+  ggplot() + geom_line(aes(x = Year, y = COUNT, col = CHAMBER))
+#Number of NAs
+Combined %>% mutate(NA_VALS = ifelse(is.na(POS_VOTE),1,0)) %>% group_by(CHAMBER) %>% summarize(NA_PERCENT = mean(NA_VALS))
+#NAs by year
+Combined %>% mutate(NA_VALS = ifelse(is.na(POS_VOTE),1,0)) %>% group_by(CHAMBER, Year) %>%
+  summarize(NA_PERCENT = mean(NA_VALS)) %>% ggplot() + geom_line(aes(x = Year, y = NA_PERCENT, col = CHAMBER))
+#NAs by vote
+Combined %>%
+  mutate(NA_VALS = ifelse(is.na(POS_VOTE),1,0)) %>% group_by(CHAMBER, Year, ROLL_CALL) %>%
+  summarize(VOTES = n(),REPS = length(unique(Member.of.Congress)), NA_PERCENT = mean(NA_VALS)) %>%
+  mutate(ROLL = Year*1000 + ROLL_CALL) %>%
+  filter(NA_PERCENT >0.15)
+#NAs by Political Party
+Combined %>%
+  mutate(NA_VALS = ifelse(is.na(POS_VOTE),1,0),
+         REP = ifelse(Party == "R", "R","D")) %>% group_by(REP) %>%
+  summarize(NA_PERCENT = mean(NA_VALS))
+By_Party
 
+#---------------------Trends in Percentages by Year and Party------------------------@
 
+ByParty_Time = Combined %>% mutate(Party = ifelse(Party == "R", "R", "D/I")) %>%
+  group_by(Party, Year, CHAMBER) %>%
+  summarize(Positive = 100*mean(POS_VOTE, na.rm = T)) %>%
+  ungroup() 
+#Plotting
+ggplot(ByParty_Time) +
+  geom_line(aes(x = Year, y = Positive, 
+                col = interaction(Party,CHAMBER),
+                lty = interaction(Party,CHAMBER))) +
+  ylab("Percent Positive Environmental Votes") +
+  scale_color_manual(name = "",
+                     values = c("blue", "red", "blue","red"),
+                     labels = c("House Democrats", "House Republicans", "Senate Democrats", "Senate Republicans")) +
+  scale_linetype_manual(name = "",
+                        values = c(1,1,2,2), 
+                        labels = c("House Democrats", "House Republicans", "Senate Democrats", "Senate Republicans")) +
+  theme_minimal() +
+  theme(legend.position = "bottom", legend.box = "vertical", legend.text = element_text(size = 11)) 
+#Calculating Partisan Divide
+ByParty_Time %>% filter(Year == 2015)
+
+#Explanatory Power of Party Only
+Combined = Combined %>%
+  mutate(REP = ifelse(Party == "R", "R", "D"))
+
+summary(lm(POS_VOTE ~ REP, data = subset(Combined, CHAMBER == "SEN")))$r.squared
 
 #-----------------------Trends in LCV Scores By Party-------------------------#
-group.colors = c(D = "blue", R = "red", I = "darkgreen")
+
 
 SEN %>%
   group_by(Party, Year) %>%
